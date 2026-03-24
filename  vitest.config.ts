4fe2f504d9edfda9301c5b1c30d9b67201824e90
cppshizoid <alex.krivoschecov@gmail.com>
2026-03-24 11:53:07 +0700

    7 lab done tests

---  vitest.config.ts
+++  vitest.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig ({
+export default defineConfig({
+    plugins: [react()],
     test: {
-        environment: 'node',
-        include: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
-        globals: false,
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'lcov', 'html'],
-            reportsDirectory: 'coverage'
-        }
-    }
-})
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/test/setup.ts'],
+        css: {
+            modules: {
+                classNameStrategy: 'non-scoped',
+            },
+        },
+        environmentOptions: {
+            jsdom: {
+                resources: 'usable',
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
